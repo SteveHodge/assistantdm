cefb0ba1c304d64364f4adc447b7b365c69f98db
Steve Hodge <stevehodge@gmail.com>
2016-05-26 01:46:29 +1200

    Removed reimplementation of equals.

--- gamesystem/dice/HDDice.java
+++ gamesystem/dice/HDDice.java
@@ -31,13 +31,6 @@ public class HDDice extends SimpleDice {
 		constant = s.constant;
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (!(o instanceof HDDice)) return false;
-		HDDice d = (HDDice) o;
-		return number == d.number && type == d.type && constant == d.constant;
-	}
-
 	public int getConstant() {
 		return constant;
 	}
