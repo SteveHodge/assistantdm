3ae9c9e049f46f5dbfb2572a70edaf790ceb5a7c
Steve Hodge <stevehodge@gmail.com>
2018-06-07 23:34:12 +1200

    Fix bug with saving throw misc mod on web sheet

--- party/CharacterSheetView.java
+++ party/CharacterSheetView.java
@@ -423,7 +423,7 @@ public class CharacterSheetView {
 			int temp = 0;
 			if (character.saveMisc.get(s.getType()) != null) temp = character.saveMisc.get(s.getType()).getModifier();
 			if (temp != 0) saveEl.setAttribute("misc", getModifierString(temp));	// the misc/temp modifier applied through the ui
-			int misc = s.getValue() - s.getRegularValue() - character.getAbilityStatistic(s.getType().getAbilityType()).getModifierValue() - temp;
+			int misc = s.getModifiersTotal() - character.getAbilityStatistic(s.getType().getAbilityType()).getModifierValue() - temp;
 			if (misc != 0) saveEl.setAttribute("mods", getModifierString(misc));	// mods is the total combined modifiers other than the misc/temp modifier and the ability modifier
 		}
 
